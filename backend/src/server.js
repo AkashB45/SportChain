a9456ce8a5e5ac22dd2911103bdc0404c63675af
AkashB45 <akashbalaji594@gmail.com>
2026-02-21 22:23:15 +0530

    server-update

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;
